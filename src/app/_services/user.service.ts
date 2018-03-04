a78cf53d59643b4b1b53edc4105a441cc3dc845f
Harpreet Singh <harpreetsingh14388@live.com>
2018-03-04 12:16:36 +0530

    angular-jwt and auth.module added

--- src/app/_services/user.service.ts
+++ src/app/_services/user.service.ts
@@ -1,6 +1,7 @@
+import { AuthHttp } from 'angular2-jwt';
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
-import { Http, RequestOptions, Headers } from '@angular/http';
+import { RequestOptions, Headers } from '@angular/http';
 import { Observable } from 'rxjs/observable';
 import { User } from '../_models/User';
 
@@ -11,22 +12,12 @@ import 'rxjs/add/observable/throw';
 @Injectable()
 export class UserService {
   baseUrl = environment.apiUrl;
-  constructor(private http: Http) { }
+  constructor(private authHttp: AuthHttp) { }
 
   getUsers(): Observable<User[]> {
-    return this.http.get(this.baseUrl + 'user', this.jwt())
+    return this.authHttp.get(this.baseUrl + 'user')
       .map(response => <User[]>response.json())
       .catch(this.handleError);
-  }
-
-  //get token
-  private jwt() {
-    let token = localStorage.getItem('token');
-    if (token) {
-      let headers = new Headers({ 'Authorization': 'Bearer ' + token });
-      headers.append('Content-type', 'application/json');
-      return new RequestOptions({ headers: headers });
-    }
   }
 
   //handle api errors
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { AuthModule } from './auth/auth.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
@@ -36,7 +37,8 @@ import { UserViewComponent } from './components/users/user-view/user-view.compon
     HttpModule,
     FormsModule,
     BsDropdownModule.forRoot(),
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    AuthModule
   ],
   providers: [AuthService, AlertifyService, AuthGuard, UserService],
   bootstrap: [AppComponent]
