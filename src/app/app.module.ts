9dcc5f2ce447c3945baa50182a7634283faee8bf
Harpreet Singh <harpreetsingh14388@live.com>
2018-03-24 14:30:35 +0530

    app module and route module updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,37 @@
-import { AuthModule } from './auth/auth.module';
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { HttpModule } from '@angular/http';
-import { FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
-import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
-import { TabsModule } from 'ngx-bootstrap/tabs';
-import { NgxGalleryModule } from 'ngx-gallery';
+import { AuthModule } from "./auth/auth.module";
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { HttpModule } from "@angular/http";
+import { FormsModule } from "@angular/forms";
+import { RouterModule, Router } from "@angular/router";
+import { BsDropdownModule } from "ngx-bootstrap/dropdown";
+import { TabsModule } from "ngx-bootstrap/tabs";
+import { NgxGalleryModule } from "ngx-gallery";
 
-import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { SignupComponent } from './components/signup/signup.component';
-import { HomeComponent } from './components/home/home.component';
-import { ListComponent } from './components/list/list.component';
-import { MessagesComponent } from './components/messages/messages.component';
-import { UserListComponent } from './components/users/user-list/user-list.component';
-import { UserViewComponent } from './components/users/user-view/user-view.component';
-import { UserDetailComponent } from './components/users/user-detail/user-detail.component';
+import { AppComponent } from "./app.component";
+import { NavbarComponent } from "./components/navbar/navbar.component";
+import { SignupComponent } from "./components/signup/signup.component";
+import { HomeComponent } from "./components/home/home.component";
+import { ListComponent } from "./components/list/list.component";
+import { MessagesComponent } from "./components/messages/messages.component";
+import { UserListComponent } from "./components/users/user-list/user-list.component";
+import { UserViewComponent } from "./components/users/user-view/user-view.component";
+import { UserDetailComponent } from "./components/users/user-detail/user-detail.component";
+import { UserEditComponent } from "./components/users/user-edit/user-edit.component";
+import { PhotoComponent } from "./components/users/photo/photo.component";
 
-import { AuthService } from './_services/auth.service';
-import { AlertifyService } from './_services/alertify.service';
-import { UserService } from './_services/user.service';
+import { AuthService } from "./_services/auth.service";
+import { AlertifyService } from "./_services/alertify.service";
+import { UserService } from "./_services/user.service";
 
-import { appRoutes } from './app.routes';
-import { AuthGuard } from './_guards/auth.guard';
-import { UserResolver } from './_resolvers/user.resolver';
-import { UsersResolver } from './_resolvers/users.resolver';
-import { UserEditComponent } from './components/users/user-edit/user-edit.component';
-import { UserEditResolver } from './_resolvers/user-edit.resolver';
-import { PreventUnsavedChanges } from './_guards/unsaved-changes.guard';
+// import { appRoutes } from "./app.routes";
+import { AuthGuard } from "./_guards/auth.guard";
+import { UserResolver } from "./_resolvers/user.resolver";
+import { UsersResolver } from "./_resolvers/users.resolver";
+import { UserEditResolver } from "./_resolvers/user-edit.resolver";
+import { PreventUnsavedChanges } from "./_guards/unsaved-changes.guard";
+import { FileUploadModule } from "ng2-file-upload";
+import { AppRoutingModule } from "./app.routes";
 
 @NgModule({
   declarations: [
@@ -41,19 +44,30 @@ import { PreventUnsavedChanges } from './_guards/unsaved-changes.guard';
     MessagesComponent,
     UserViewComponent,
     UserDetailComponent,
-    UserEditComponent
+    UserEditComponent,
+    PhotoComponent
   ],
   imports: [
     BrowserModule,
     HttpModule,
     FormsModule,
     BsDropdownModule.forRoot(),
-    RouterModule.forRoot(appRoutes),
     AuthModule,
     TabsModule.forRoot(),
-    NgxGalleryModule
+    NgxGalleryModule,
+    FileUploadModule,
+    AppRoutingModule
   ],
-  providers: [AuthService, AlertifyService, AuthGuard, PreventUnsavedChanges, UserService, UserResolver, UsersResolver, UserEditResolver],
+  providers: [
+    AuthService,
+    AlertifyService,
+    AuthGuard,
+    PreventUnsavedChanges,
+    UserService,
+    UserResolver,
+    UsersResolver,
+    UserEditResolver
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,26 +1,49 @@
-import { Routes } from '@angular/router';
-import { HomeComponent } from './components/home/home.component';
-import { ListComponent } from './components/list/list.component';
-import { MessagesComponent } from './components/messages/messages.component';
-import { AuthGuard } from './_guards/auth.guard';
-import { UserListComponent } from './components/users/user-list/user-list.component';
-import { UserDetailComponent } from './components/users/user-detail/user-detail.component';
-import { UserResolver } from './_resolvers/user.resolver';
-import { UsersResolver } from './_resolvers/users.resolver';
-import { UserEditComponent } from './components/users/user-edit/user-edit.component';
-import { UserEditResolver } from './_resolvers/user-edit.resolver';
-import { PreventUnsavedChanges } from './_guards/unsaved-changes.guard';
+import { Routes, RouterModule, Router } from "@angular/router";
+import { HomeComponent } from "./components/home/home.component";
+import { ListComponent } from "./components/list/list.component";
+import { MessagesComponent } from "./components/messages/messages.component";
+import { AuthGuard } from "./_guards/auth.guard";
+import { UserListComponent } from "./components/users/user-list/user-list.component";
+import { UserDetailComponent } from "./components/users/user-detail/user-detail.component";
+import { UserResolver } from "./_resolvers/user.resolver";
+import { UsersResolver } from "./_resolvers/users.resolver";
+import { UserEditComponent } from "./components/users/user-edit/user-edit.component";
+import { UserEditResolver } from "./_resolvers/user-edit.resolver";
+import { PreventUnsavedChanges } from "./_guards/unsaved-changes.guard";
+import { NgModule } from "@angular/core";
 
-export const appRoutes: Routes = [
-    { path: 'home', component: HomeComponent },
-    {
-        path: '', runGuardsAndResolvers: 'always', canActivate: [AuthGuard], children: [
-            { path: 'users', component: UserListComponent, resolve: { users: UsersResolver } },
-            { path: 'users/:id', component: UserDetailComponent, resolve: { user: UserResolver } },
-            { path: 'user/:id', component: UserEditComponent, resolve: { user: UserEditResolver }, canDeactivate: [PreventUnsavedChanges] },
-            { path: 'lists', component: ListComponent },
-            { path: 'messages', component: MessagesComponent },
-        ]
-    },
-    { path: '**', redirectTo: 'home', pathMatch: 'full' }
-]
+const routes: Routes = [
+  { path: "home", component: HomeComponent },
+  {
+    path: "",
+    runGuardsAndResolvers: "always",
+    canActivate: [AuthGuard],
+    children: [
+      {
+        path: "users",
+        component: UserListComponent,
+        resolve: { users: UsersResolver }
+      },
+      {
+        path: "users/:id",
+        component: UserDetailComponent,
+        resolve: { user: UserResolver }
+      },
+      {
+        path: "user/:id",
+        component: UserEditComponent,
+        resolve: { user: UserEditResolver },
+        canDeactivate: [PreventUnsavedChanges]
+      },
+      { path: "lists", component: ListComponent },
+      { path: "messages", component: MessagesComponent }
+    ]
+  },
+  { path: "**", redirectTo: "home", pathMatch: "full" }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
