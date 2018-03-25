6ca59cc61945dec360106c6bbfeebd31d7db3201
Harpreet Singh <harpreetsingh14388@live.com>
2018-03-25 16:33:50 +0530

    a method to set profile pic added

--- src/app/_services/user.service.ts
+++ src/app/_services/user.service.ts
@@ -1,57 +1,66 @@
-import { AuthHttp } from 'angular2-jwt';
-import { Injectable } from '@angular/core';
-import { environment } from '../../environments/environment';
-import { RequestOptions, Headers } from '@angular/http';
-import { Observable } from 'rxjs/observable';
-import { User } from '../_models/User';
+import { AuthHttp } from "angular2-jwt";
+import { Injectable } from "@angular/core";
+import { environment } from "../../environments/environment";
+import { RequestOptions, Headers } from "@angular/http";
+import { Observable } from "rxjs/observable";
+import { User } from "../_models/User";
 
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/catch';
-import 'rxjs/add/observable/throw';
+import "rxjs/add/operator/map";
+import "rxjs/add/operator/catch";
+import "rxjs/add/observable/throw";
 
 @Injectable()
 export class UserService {
   baseUrl = environment.apiUrl;
-  constructor(private authHttp: AuthHttp) { }
+  constructor(private authHttp: AuthHttp) {}
 
+  //get users
   getUsers(): Observable<User[]> {
     return this.authHttp
-      .get(this.baseUrl + 'user')
+      .get(this.baseUrl + "user")
       .map(response => <User[]>response.json())
       .catch(this.handleError);
   }
 
+  //get user
   getUser(id): Observable<User> {
     return this.authHttp
-      .get(this.baseUrl + 'user/' + id)
+      .get(this.baseUrl + "user/" + id)
       .map(response => <User>response.json())
       .catch(this.handleError);
   }
 
+  //update user
   updateUser(id: Number, user: User) {
-    return this.authHttp.put(this.baseUrl + 'user/' + id, user)
+    return this.authHttp
+      .put(this.baseUrl + "user/" + id, user)
+      .catch(this.handleError);
+  }
+  //profile pic
+  setProfilePicture(userId: Number, photoId: Number) {
+    return this.authHttp
+      .post(
+        this.baseUrl + "users/" + userId + "/photos/" + photoId + "/setMain",
+        {}
+      )
       .catch(this.handleError);
   }
 
   //handle api errors
   private handleError(error: any) {
     //application error
-    const applicationError = error.headers
-      .get('Application-Error');
-    if (applicationError) return Observable
-      .throw(applicationError);
+    const applicationError = error.headers.get("Application-Error");
+    if (applicationError) return Observable.throw(applicationError);
     //server error
     const serverError = error.json();
-    let modelStateErrors = '';
+    let modelStateErrors = "";
     if (serverError) {
       for (let key in serverError) {
         if (serverError[key]) {
-          modelStateErrors += serverError[key] + '\n';
+          modelStateErrors += serverError[key] + "\n";
         }
       }
     }
-    return Observable.throw(
-      modelStateErrors || 'Server error'
-    );
+    return Observable.throw(modelStateErrors || "Server error");
   }
 }
